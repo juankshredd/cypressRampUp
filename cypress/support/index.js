e62242998025bc929290c79bc37809b34cc2d5d0
Juan Bohorquez Marcelo <juan.bohorquezm@perficient.com>
2022-06-23 17:23:02 -0500

    se corrige error de linea vacia y se prepara para cucumber

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -14,6 +14,7 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
+require('cypress-dark')
 import './commands'
 
 // Alternatively you can use CommonJS syntax:
